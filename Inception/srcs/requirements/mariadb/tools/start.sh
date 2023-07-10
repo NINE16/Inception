7aaf87abd3627fc7a463de16c014c14ddd923162
NINE16 <74472310+NINE16@users.noreply.github.com>
2023-07-10 11:16:11 +0200

    Add files via upload

---
+++ Inception/srcs/requirements/mariadb/tools/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mysql_install_db
+
+/etc/init.d/mysql start
+
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+
+/etc/init.d/mysql stop
+
+exec "$@"
---
+++ Inception/srcs/requirements/wordpress/conf/w.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -x
+sleep 5
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
+	#Downloading wordpress and all config file
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* /var/www/html
+	rm -rf latest.tar.gz
+	rm -rf wordpress
+	chown -R www-data:www-data /var/www/
+	chmod -R 755 /var/www/html/
+
+	wp config create	--allow-root \
+						--dbname=$MYSQL_DATABASE \
+						--dbuser=$MYSQL_USER \
+						--dbpass=$MYSQL_PASSWORD \
+						--dbhost=$MYSQL_HOSTNAME --path='/var/www/html'
+	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root --path='/var/www/html'
+	wp user create $MYSQL_USER1 $MYSQL_USER_MAIL1 --user_pass=$MYSQL_PASSWORD1 --role=author --allow-root --path='/var/www/html'
+	wp theme install zino --activate --allow-root --path='/var/www/html'
+fi
+if [ ! -d /run/php ]; then
+    mkdir -p /run/php
+fi
+/usr/sbin/php-fpm7.3 -F
+
+exec "$@"
